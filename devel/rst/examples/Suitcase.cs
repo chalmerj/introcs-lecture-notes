38a46a7212838f92145fc466be417f64bf652bd6
Andrew N. Harrington <anharrington@gmail.com>
2012-02-01 16:41:15 -0600

    typo in string in Suitcse.cs

--- devel/rst/examples/Suitcase.cs
+++ devel/rst/examples/Suitcase.cs
@@ -4,7 +4,7 @@ class Suitcase
 {
    static void Main()
    {                                        //main chunk
-      Console.Write ( "How many pounds does you suitcase weigh? ");
+      Console.Write ( "How many pounds does your suitcase weigh? ");
       double weight = double.Parse(Console.ReadLine());
       if (weight > 50) {
            Console.WriteLine("There is a $25 charge for luggage that heavy.");
