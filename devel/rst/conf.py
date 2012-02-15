8220a8cb5230717913ab23e0932f479f1f4e6f37
George K. Thiruvathukal <thiruvathukal@gmail.com>
2012-02-15 09:23:51 -0600

    testing a new theme based on Twitter bootstrap framework

--- devel/rst/conf.py
+++ devel/rst/conf.py
@@ -108,22 +108,23 @@ rst_epilog = """
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'bootstrap'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = { 'collapsiblesidebar' : True }
+#html_theme_options = { 'collapsiblesidebar' : True }
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = ['_themes']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
 #html_title = None
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-html_short_title = 'C# Intro'
+#html_short_title = 'C# Intro'
+html_short_title = 'COMP 170'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
